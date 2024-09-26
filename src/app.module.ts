a802f08d5954facb5d248a747347da02ea274fbf
whyy <icarovsilva1@gmail.com>
2024-09-26 12:38:40 -0300

    feat: init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config'
 import { DatabaseModule } from './database/database.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ThrottlerModule } from '@nestjs/throttler'
 
 @Module({
   imports: [
@@ -13,6 +14,24 @@ import { JwtModule } from '@nestjs/jwt';
       secret: process.env.JWT_SECRET,
       signOptions: {expiresIn: "1d"}
     }),
+    ThrottlerModule.forRoot([
+      {
+        name: "short",
+        ttl: 1000,
+        limit: 3
+      },
+      {
+        name: 'medium',
+        ttl:1000,
+        limit: 20
+      },
+      {
+        name: "long",
+        ttl: 60000,
+        limit: 100
+      }
+    ])
+    ,
     DatabaseModule,
   ],
   controllers: [],
